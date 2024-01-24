38ac9e375faa6497ff0d224db337907c03c62c4c
Kuwujii <kuwujii@gmail.com>
2024-01-24 17:21:32 +0100

    Unignore some stuff to make stuff easier for future me

---
+++ compile_shaders.bat
@@ -0,0 +1,2 @@
+C:\VulkanSDK\1.3.211.0\Bin\glslc.exe src\shaders\simple.vert -o build\Debug\shaders\simple.vert.spv
+C:\VulkanSDK\1.3.211.0\Bin\glslc.exe src\shaders\simple.frag -o build\Debug\shaders\simple.frag.spv
